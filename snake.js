6a0d003faf79ae82dbdd031adecf5585a4d499dd
Ivan Yeremenko <i.p.yeremenko@gmail.com>
2022-06-18 18:35:05 +0200

    Some new mechanics and speed adjustments

--- snake.js
+++ snake.js
@@ -1,6 +1,7 @@
 import { getInputDirection } from "./input.js";
 
-export const snakeSpeed = 8;
+export let snakeSpeed = 4;
+let flag = 0;
 const centerOfTheMap = 11;
 const snakeBody = [{x : centerOfTheMap, y : centerOfTheMap}];
 let newSegments = 0;
@@ -9,7 +10,7 @@ export const update = () => {
 	addSegments();
   const inputDirection = getInputDirection();
   for(let i = snakeBody.length - 2; i >= 0; i--) {
-      snakeBody[i + 1] = { ...snakeBody[i] }
+    snakeBody[i + 1] = { ...snakeBody[i] }
   }
 
   snakeBody[0].x += inputDirection.x;
@@ -17,17 +18,22 @@ export const update = () => {
 };
 
 export const draw = (map) => {
-    snakeBody.forEach(segment => {
-        const snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = segment.y;
-        snakeElement.style.gridColumnStart = segment.x;
-        snakeElement.classList.add('snake');
-        map.appendChild(snakeElement);
-    })
+  snakeBody.forEach(segment => {
+    const snakeElement = document.createElement('div');
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.classList.add('snake');
+    map.appendChild(snakeElement);
+  })
 };
 
-export const expandSnake = (expAmount) => {
-    newSegments += expAmount;
+export const expandSnake = (expAmount, expSpeed) => {
+	flag += 1;
+	newSegments += expAmount;
+	if (flag % 5 === 0) {
+    snakeSpeed += expSpeed;
+  }
+  
 }
 
 export const onSnake = (position, { ignoreHead = false } = {} ) => {
@@ -45,8 +51,8 @@ export const snakeIntersection = () => {
 	return onSnake(snakeBody[0], { ignoreHead: true})
 }
 
-const equalPositions = (snakePos, berryPos) => {
-	return (snakePos.x === berryPos.x && snakePos.y === berryPos.y);
+const equalPositions = (snakePos, itemPos) => {
+	return (snakePos.x === itemPos.x && snakePos.y === itemPos.y);
 }
 
 const addSegments = () => {
@@ -57,3 +63,10 @@ const addSegments = () => {
 	newSegments = 0;
 }
 
+export const speedDown = (downAmount) => {
+  snakeSpeed = downAmount;
+}
+
+export const speedUp = (upAmount) => {
+  snakeSpeed = upAmount;
+}
